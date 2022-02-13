8451a2b04f2d2d7ffc1c33c4623e12debdb04d0a
Fadin Vladimir <fadinvv95@gmail.com>
2022-02-13 21:00:16 +0300

    created

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
+import os
+
 from pyrogram import Client, filters
+from dotenv import load_dotenv, find_dotenv
+import requests
 
 app = Client("my_account")
 
+load_dotenv(find_dotenv())
+channel_id = int(os.getenv('CHANNEL_ID'))
+bot_id = int(os.getenv('BOT_ID'))
 
 @app.on_message()
 async def hello(client, message):
-    me = await app.get_me()
-    # Если автор сообщения я - выйдем
-    if message.from_user and message.from_user.id == me.id:
-        return
-    # Говнокод, надо разобраться, как безопасно сделать эту проверку
-    if message.chat.id == int(app.config_file.CHANNEL_ID):
-        return
-    if message.chat.id == int(app.config_file.BOT_ID):
-        return
-    # Если это уведомление о созданном канале выходим
-    if message.channel_chat_created:
-        return
-    # Наше сообщение, которое мы будем отправлять боту
-    text = message.text
-    # Если есть текст выполняем код
-    if text:
-        # Массив значений стиля текста, библиотечка сама рисует так как нужно
-        # Отправляем сообщение боту, который потом будет переводить и писать в БД
-        await app.send_message(int(app.config_file.BOT_ID), text, entities=message.entities)
+    body = {"query": "{\n\t\t\tgetSendChats {\n\t\t\t\tid\n\t\t\t\ttgChatId\n\t\t\t    listen\n\t\t\t}\n\t\t}"}
+    res = requests.post('http://localhost:8000/graphql', json=body)
+    send_chats = []
+    get_send_chats = res.json()['data']['getSendChats']
+    for i in range(len(get_send_chats)):
+        send_chats.append(int(get_send_chats[i]['tgChatId']))
+    if message.chat.id in send_chats:
+        # Если автор сообщения я - выйдем
+        if message.channel_chat_created:
+            return
+        # Наше сообщение, которое мы будем отправлять боту
+        text = message.text
+        # Если есть текст выполняем код
+        if text:
+            # Массив значений стиля текста, библиотечка сама рисует так как нужно
+            # Отправляем сообщение боту, который потом будет переводить и писать в БД
+            await app.send_message(bot_id, text, entities=message.entities)
+
+        photo = message.photo
+        caption = message.caption
+        if photo:
+            await app.send_photo(bot_id, photo=photo.file_id, caption=caption, caption_entities=message.caption_entities)
 
 
 app.run()
